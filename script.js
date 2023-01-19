936686c29b745b965b6cb745154e4282edc4e450
lamauvaisepente <123085273+lamauvaisepente@users.noreply.github.com>
2023-01-19 14:26:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("formulaire").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Merci pour votre message ! Un membre du groupe vous répondra dès que possible.");
+});
